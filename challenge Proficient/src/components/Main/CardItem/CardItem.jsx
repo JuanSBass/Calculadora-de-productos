752c30ef92acc18d99e68f23983ab15d9fb29a20
JuanSBass <juan_santillan20@hotmail.com>
2022-11-27 22:15:44 -0600

    add icons cards + render

---
+++ challenge Proficient/src/components/Main/CardItem/CardItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./carditem.module.css"
+
+export const CardItem = ({obj, img}) => {
+  return (
+    <div className={styles.cardicon}>
+      <img src={img} alt={obj} />
+      <h3>{obj}</h3>
+    </div>
+  )
+}
--- challenge Proficient/src/components/Main/Main.jsx
+++ challenge Proficient/src/components/Main/Main.jsx
@@ -1,13 +1,77 @@
 import React from 'react';
+import { CardItem } from './CardItem/CardItem';
 import styles from "./main.module.css";
+import beads from "../../Img/Beads.png";
+import desk from "../../Img/Desk.png";
+import dining from "../../Img/Dining.png";
+import furniture from "../../Img/Furniture.png";
+import oven from "../../Img/Furniture.png";
+import refri from "../../Img/refridegator.png";
+import sofa from "../../Img/Sofa.png";
+import tv from "../../Img/TV.png";
+import wardrobe from "../../Img/Wardrobe.png";
+import washer from "../../Img/Washer.png";
 
 export const Main = () => {
   return (
-    <div className={styles.header}>
+    <main className={styles.header}>
       <h1>What items to store?</h1>
       <p>Select which items you wish to store before moving to your new home. We'll keep 'em safe!</p>
-    </div>
+      <section className={styles.cardscontainer}>
+        {
+          arrItems?.map(item => (
+            <CardItem obj={item.obj} img={item.img}/>
+          ))
+        }
+      </section>
+    </main>
   )
 }
 
+const arrItems = [
+  {
+    obj: "Beads",
+    img: beads
+  },
+  {
+    obj: "Refrigerador",
+    img: refri
+  },
+  {
+    obj: "Furniture",
+    img: furniture
+  },
+  {
+    obj: "Oven",
+    img: oven
+  },
+  {
+    obj: "Sofa",
+    img: sofa
+  },
+  {
+    obj: "Beads",
+    img: beads
+  },
+  {
+    obj: "TV",
+    img: tv
+  },
+  {
+    obj: "Washer-dryer",
+    img: washer
+  },
+  {
+    obj: "Dining",
+    img: dining
+  },
+  {
+    obj: "Desk",
+    img: desk
+  },
+  {
+    obj: "Wardrobe",
+    img: wardrobe
+  },
+]
 
