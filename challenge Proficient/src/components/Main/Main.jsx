9ed8cdb88e86f21e55d5f0d940ec58ed34c36f38
JuanSBass <juan_santillan20@hotmail.com>
2022-11-27 21:32:32 -0600

    header-text ok

--- challenge Proficient/src/components/Main/Main.jsx
+++ challenge Proficient/src/components/Main/Main.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
+import styles from "./main.module.css";
 
 export const Main = () => {
   return (
-    <div>Main</div>
+    <div className={styles.header}>
+      <h1>What items to store?</h1>
+      <p>Select which items you wish to store before moving to your new home. We'll keep 'em safe!</p>
+    </div>
   )
 }
+
+
