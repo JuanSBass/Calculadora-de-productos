7d12227a991edc8432dc5970b64090196ec2a075
JuanSBass <juan_santillan20@hotmail.com>
2022-11-30 18:53:01 -0600

    estados + botones ok

--- challenge Proficient/src/components/Main/Lateralbuttons/Lateralbuttons.jsx
+++ challenge Proficient/src/components/Main/Lateralbuttons/Lateralbuttons.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import styles from "./lateralbuttons.module.css"
 
-export const Lateralbuttons = () => {
+export const Lateralbuttons = ({clear}) => {
   return (
     <div className={styles.lateralbtnscontainer}>
-      <button id={styles.clearbtn}>Clear</button>
+      <button id={styles.clearbtn} onClick={clear}>Clear</button>
       <button id={styles.calculatebtn}>Calculate</button>
     </div>
   )
--- challenge Proficient/src/components/Main/Summary/Summary.jsx
+++ challenge Proficient/src/components/Main/Summary/Summary.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import styles from "./summary.module.css";
 
-export const Summary = () => {
+export const Summary = ({
+  totalItems,
+  m2totales,
+  subtotal,
+  tax,
+  total,
+  dueToday,
+}) => {
   return (
     <section className={styles.summarycontainer}>
       <h1>Summary</h1>
@@ -16,15 +23,21 @@ export const Summary = () => {
           <p>
             <b>Total</b>
           </p>
-          <p><b>Due Today 50%</b></p>
+          <p>
+            <b>Due Today 50%</b>
+          </p>
         </div>
         <div className={styles.resultlist}>
-          <p>12</p>
-          <p>8.55</p>
-          <p>$1.710</p>
-          <p>$273.6</p>
-          <p><b>$1,983.6</b></p>
-          <p><b>$991.8</b></p>
+          <p>{totalItems}</p>
+          <p>{m2totales}</p>
+          <p>{subtotal}</p>
+          <p>${tax}</p>
+          <p>
+            <b>${total}</b>
+          </p>
+          <p>
+            <b>${dueToday}</b>
+          </p>
         </div>
       </div>
     </section>
